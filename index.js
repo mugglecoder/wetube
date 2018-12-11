7faa861de272a45c12e75ddf5e768c431ece0130
Park <winkknd@naver.com>
2018-12-11 10:45:31 +0900

    Initial Commit

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.listen(4000);
+const PORT = 4000;
+
+function handleListen() {
+  console.log(`Listening on PORT http://localhost:${PORT}`);
+}
+
+function handleHome(req, res) {
+  res.send("welcome Home");
+}
+
+app.get("/", handleHome);
+
+app.listen(PORT, handleListen);
