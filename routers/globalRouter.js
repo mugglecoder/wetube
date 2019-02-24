31c8d4ca6e9b2fd6a4e09e50c93cc944d4d59892
Park <winkknd@naver.com>
2019-02-24 16:29:05 +0900

    Initial Commit

---
+++ routers/globalRouter.js
@@ -0,0 +1,25 @@
+import express from "express";
+import routes from "../routes";
+import { home, search } from "../controllers/videoController";
+import {
+  getJoin,
+  getLogin,
+  logout,
+  postJoin,
+  postLogin
+} from "../controllers/userController";
+import { onlyPublic } from "../middlewares";
+
+const globalRouter = express.Router();
+
+globalRouter.get(routes.join, onlyPublic, getJoin);
+globalRouter.post(routes.join, onlyPublic, postJoin, postLogin);
+
+globalRouter.get(routes.login, onlyPublic, getLogin);
+globalRouter.post(routes.login, onlyPublic, postLogin);
+
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.search, search);
+globalRouter.get(routes.logout, logout);
+
+export default globalRouter;
